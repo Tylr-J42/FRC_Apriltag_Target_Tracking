429f6158f90017ac1f776e9a4f5856bbec335061
Tyler Jacques <tyler.jacques00@gmail.com>
2024-12-19 23:30:50 -0500

    swerve tracking

--- Apriltag_Tracking_Demo/src/main/java/frc/robot/RobotContainer.java
+++ Apriltag_Tracking_Demo/src/main/java/frc/robot/RobotContainer.java
@@ -4,13 +4,15 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import frc.robot.Subsystems.Drivetrain;
-import frc.robot.Subsystems.Vision;
+import frc.robot.constants.OIConstants;
+import frc.robot.subsystems.Drivetrain;
+import frc.robot.subsystems.Vision;
 
 public class RobotContainer {
     private CommandXboxController driver;
@@ -22,7 +24,7 @@ public class RobotContainer {
     
     public RobotContainer() {
         driver = new CommandXboxController(0);
-        drivetrain = new Drivetrain();
+        drivetrain = new Drivetrain(new Pose2d());
         vision = new Vision();
 
         configureBindings();
@@ -37,19 +39,21 @@ public class RobotContainer {
 
         autoTab.addDouble("tx", vision::getTag3tx);
         autoTab.addDouble("ty", vision::getTag3ty);
-        autoTab.addDouble("Output Voltage", drivetrain::getTurningPIDOutput);
     }
 
     private void configureBindings() {
-        drivetrain.setDefaultCommand(drivetrain.arcadeDrive(
-            () -> -driver.getLeftY(), 
-            () -> driver.getRightX()));
-
-        driver.a().onTrue(drivetrain.trackApriltag(vision::getTag3ty));
+        drivetrain.setDefaultCommand(
+            drivetrain.teleopCommand(
+                () -> { return driver.getLeftY();}, 
+                () -> { return driver.getLeftX();}, 
+                () -> { return driver.getRightX();}, 
+                OIConstants.kTeleopDriveDeadband
+            )
+        );
     }
 
-    public Command getAutonomousCommand() {
-        return drivetrain.trackApriltag(vision::getTag3tx);
-    }
+    //public Command getAutonomousCommand() {
+     //   return drivetrain.trackApriltag(vision::getTag3tx);
+    //}
 
 }
---
+++ Apriltag_Tracking_Demo/src/main/java/frc/robot/constants/OIConstants.java
@@ -0,0 +1,7 @@
+package frc.robot.constants;
+
+public class OIConstants {
+    public static final int kPrimaryXboxUSB = 0;
+    public static final int kSecondaryXboxUSB = 1;
+    public static final double kTeleopDriveDeadband = 0.05;
+}
