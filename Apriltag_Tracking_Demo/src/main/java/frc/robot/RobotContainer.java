64d922b83b07f4f7f05006e800e7f8bc15eb9c77
Tyler Jacques <tyler.jacques00@gmail.com>
2024-12-19 23:39:42 -0500

    renamed folder

--- Apriltag_Tracking_Demo/src/main/java/frc/robot/RobotContainer.java
+++ Apriltag_Tracking_Demo/src/main/java/frc/robot/RobotContainer.java
@@ -9,8 +9,8 @@ import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import frc.robot.Subsystems.Drivetrain;
-import frc.robot.Subsystems.Vision;
+import frc.robot.subsystems.Drivetrain;
+import frc.robot.subsystems.Vision;
 
 public class RobotContainer {
     private CommandXboxController driver;
